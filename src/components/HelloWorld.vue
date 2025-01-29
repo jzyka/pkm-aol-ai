5e447dc49e201d80b9ec80fe5e9dd6f4db17c6bd
unknown <auliazykha@gmai.com>
2025-01-29 17:20:42 +0700

    feat [ZA]: chat success

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,188 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <v-app>
+    <v-container style="max-height: 100vh; height: 100%">
+      <div class="contain-all">
+        <div class="header">
+          <v-img src="@/assets/logo.png" contain class="logo"></v-img>
+        </div>
+        <div class="chat-container">
+          <p class="banner" v-if="chatSession == null">
+            What’s in your fridge?
+          </p>
+          <div class="parent-text" v-else>
+            <div
+              class="chat-chip"
+              v-for="(message, index) in messages"
+              :key="index"
+            >
+              <p v-html="formatMessage(message.text)"></p>
+            </div>
+          </div>
+        </div>
+        <div class="field-container">
+          <v-text-field
+            v-model="input"
+            placeholder=" List your ingredients, I’ll suggest eco-friendly meals!"
+            variant="solo"
+            base-color="#818A91"
+            bg-color="#212121"
+            color="#787878"
+            append-inner-icon="mdi-send"
+            @click:append-inner="sendMessage"
+          ></v-text-field>
+        </div>
+      </div>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      input: "",
+      messages: [],
+      chatSession: null,
+    };
+  },
+  methods: {
+    async sendMessage() {
+      if (!this.input.trim()) return;
+
+      this.messages.push({ role: "user", text: this.input });
+      const userInput = this.input;
+      this.input = "";
+
+      const apiKey = "AIzaSyCxRdnQkz2Lef12AxcUpnJIL0FdqrRHICw";
+      const genAI = new GoogleGenerativeAI(apiKey);
+      const model = genAI.getGenerativeModel({
+        model: "tunedModels/finetunerecipes-gy2de66m677",
+      });
+
+      const generationConfig = {
+        temperature: 1,
+        topP: 0.95,
+        topK: 64,
+        maxOutputTokens: 8192,
+        responseMimeType: "text/plain",
+      };
+
+      if (!this.chatSession) {
+        this.chatSession = model.startChat({
+          generationConfig,
+          history: [],
+        });
+      }
+
+      try {
+        const result = await this.chatSession.sendMessage(userInput);
+        this.messages.push({ role: "Bot", text: result.response.text() });
+      } catch (error) {
+        console.error("Error sending message:", error);
+        this.messages.push({
+          role: "Bot",
+          text: "Error processing your request.",
+        });
+      }
+    },
+    formatMessage(text) {
+      // Convert new lines to <br>
+      text = text.replace(/\n/g, "<br>");
+
+      // Convert bold markdown (**text**) to <strong>text</strong>
+      text = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+
+      // Convert italic markdown (*text*) to <em>text</em>
+      text = text.replace(/\*(.*?)\*/g, "<em>$1</em>");
+
+      // Convert markdown lists (- item) to <ul><li>item</li></ul>
+      text = text.replace(/- (.*?)<br>/g, "<li>$1</li>");
+      text = text.replace(/(<li>.*?<\/li>)+/g, "<ul>$&</ul>");
+
+      return text;
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.contain-all {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .header {
+    display: flex;
+    justify-content: flex-start;
+
+    .logo {
+      height: 40px;
+      max-width: 200px;
+    }
+  }
+
+  .chat-container {
+    height: 75%;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+
+    .banner {
+      background: linear-gradient(90deg, #0198b0 0%, #00cf65 100%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      font-size: 48px;
+      text-align: center;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      margin: auto;
+    }
+
+    .parent-text {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      padding-right: 20px;
+
+      .chat-chip {
+        max-width: 75%;
+        width: fit-content;
+        padding: 10px 25px;
+        border-radius: 20px;
+
+        &:nth-child(odd) {
+          background-color: #787878;
+          margin-left: auto;
+
+          p {
+            color: #ffffff;
+          }
+        }
+
+        &:nth-child(even) {
+          background-color: #212121;
+
+          p {
+            color: #ffffff;
+          }
+        }
+
+        // p {
+        //   text-wrap:
+        // }
+      }
+    }
+  }
+
+  .field-container {
+    // margin-bottom: 40px;
+  }
 }
 </style>
